63e3becc81dc9e7fbcb810f03a64abbd7baacd12
Sathish Swamy <145578184+SATHISH-SWAMY@users.noreply.github.com>
2025-09-07 23:07:48 +0530

    Update db.js

--- server2/config/db.js
+++ server2/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connection = async () => {
-  const DB_URL = 'mongodb://localhost:27017/retailcars';
+  const DB_URL = 'mongodb+srv://sathishfabdiz_db_user:FF4OG8vCJBNXmJTL@tn33cars.ploesny.mongodb.net/retailcars?retryWrites=true&w=majority&appName=tn33cars';
   try {
     await mongoose.connect(DB_URL);
     console.log('Database connected successfully');
